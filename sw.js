96fad130eb2596feaf06076beff43fc900ba5572
Felisias <159609165+Felisias@users.noreply.github.com>
2026-01-25 19:16:57 +0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME = 'healthflow-workouts-v1';
-const urlsToCache = [
-    '/healthflow/',
-    '/healthflow/index.html',
-    '/healthflow/style.css',
-    '/healthflow/app.js',
-    '/healthflow/manifest.json'
-];
